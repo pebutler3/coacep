d89dd2f8478aca2331b179ae15926e1f1aaa1ce9
P.E. Butler III <p.e.butler3@gmail.com>
2015-11-10 12:18:11 -0700

    added #menu for easier targeting // added new js for nav updating/scrolling

--- js/app.js
+++ js/app.js
@@ -1,40 +1,38 @@
 $(document).foundation();
-$(document).ready(function () {
+  $(document).ready(function () {
     $(document).on("scroll", onScroll);
-    
-    //smoothscroll
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-        $(document).off("scroll");
-        
-        $('a').each(function () {
-            $(this).removeClass('active');
-        })
-        $(this).addClass('active');
-      
-        var target = this.hash,
-            menu = target;
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top + 2 
-        }, 500, 'swing', function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
+ 
+    $('#menu a[href^="#"]').on('click', function (e) {
+      e.preventDefault();
+      $(document).off("scroll");
+ 
+      $('#menu a').each(function () {
+        $(this).removeClass('active');
+      })
+      $(this).addClass('active');
+ 
+      var target = this.hash;
+      $target = $(target);
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top+2
+      }, 500, 'swing', function () {
+        window.location.hash = target;
+        $(document).on("scroll", onScroll);
+      });
     });
-});
-
-function onScroll(event){
-    var scrollPos = $(document).scrollTop();
-    $('.top-bar-section > a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('.top-bar-section ul li a').removeClass("active");
-            currLink.addClass("active");
-        }
-        else{
-            currLink.removeClass("active");
-        }
+  });
+ 
+  function onScroll(event){
+    var scrollPosition = $(document).scrollTop();
+    $('#menu a').each(function () {
+      var currentLink = $(this);
+      var refElement = $(currentLink.attr("href"));
+   if(refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+        $('#menu ul li a').removeClass("active");
+        currentLink.addClass("active");
+      }
+      else{
+        currentLink.removeClass("active");
+      }
     });
-}
+  }
