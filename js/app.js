aa40e17a0b30195083878a9d7e52341ee05271dc
Paul Butler III <p.e.butler3@gmail.com>
2020-12-31 00:49:33 -0700

    feat: adds 2020 annual meeting sidebar and info

--- js/app.js
+++ js/app.js
@@ -12,13 +12,36 @@ $(function(){
   });
 
   $.scrollIt({
-    upKey: 38,             // key code to navigate to the next section
-    downKey: 40,           // key code to navigate to the previous section
-    easing: 'linear',      // the easing function for animation
-    scrollTime: 600,       // how long (in ms) the animation takes
-    activeClass: 'active', // class given to the active nav element
-    onPageChange: null,    // function(pageIndex) that is called when page is changed
+    upKey: 38,               // key code to navigate to the next section
+    downKey: 40,             // key code to navigate to the previous section
+    easing: 'linear',        // the easing function for animation
+    scrollTime: 600,         // how long (in ms) the animation takes
+    activeClass: 'active',   // class given to the active nav element
+    onPageChange: null,      // function(pageIndex) that is called when page is changed
     topOffset: -75           // offste (in px) for fixed top navigation
   });
-
 });
+
+var moreInfoButton = document.querySelector('.more-info');
+var annualMeetingSidebar = document.querySelector('.annual-meeting-sidebar');
+var annualMeetingSidebarOverlay = document.querySelector('.annual-meeting-sidebar__overlay');
+var annualMeetingSidebarOverlayOpen = document.querySelector('.annual-meeting-sidebar__overlay--open');
+var closeAnnualMeetingSidebarButton = document.querySelector('.close-annual-meeting-sidebar');
+var sidebarIsOpen = false;
+
+// moreInfoButton
+
+var closeAnnualMeetingSidebar = function() {
+  annualMeetingSidebar.classList.remove('annual-meeting-sidebar--open');
+  annualMeetingSidebarOverlay.classList.remove('annual-meeting-sidebar__overlay--open');
+}
+
+var toggleAnnualMeetingSidebar = function() {
+  annualMeetingSidebar.classList.toggle('annual-meeting-sidebar--open');
+  annualMeetingSidebarOverlay.classList.toggle('annual-meeting-sidebar__overlay--open');
+
+  annualMeetingSidebarOverlay.addEventListener('click', closeAnnualMeetingSidebar);
+  closeAnnualMeetingSidebarButton.addEventListener('click', closeAnnualMeetingSidebar);
+};
+
+moreInfoButton.addEventListener('click', toggleAnnualMeetingSidebar);
